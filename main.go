2c07b59cabd87518c0baa8316ead720cba0f7b8b
shreyas <shreyasmodedev@gmail.com>
2026-09-03 19:08:17 +0530

    First commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+	// gin router with default middleware
+	r := gin.Default()
+
+	// GET endpoint
+	r.GET("/task4", func(c *gin.Context){
+		//Return a JSON response
+		c.JSON(http.StatusOK, gin.H{
+			"message" : "This is a task4 app",
+		})
+	})
+	fmt.Println("Started the task4 go application on port 8080")
+	r.Run()
+}
